caecc640e2b49f76254b14eb622e8d87e23363d7
LeCapot <broussonm@gmail.com>
2021-10-11 17:20:55 -0300

    web server template v2

--- app.js
+++ app.js
@@ -14,23 +14,23 @@ hbs.registerPartials( __dirname + '/views/partials');
 app.use(express.static('public'));
 app.use(express.static('views'));
 
-app.get('/', (req,res) => {
-    res.render('home', {
-        nombre: 'Gonzalo',
-        titulo: 'Curso de node'
-    });
-});
+// app.get('/', (req,res) => {
+//     res.render('home', {
+//         nombre: 'Gonzalo',
+//         titulo: 'Curso de node'
+//     });
+// });
 
 
-app.get('/generic', (req,res) => {
-   // res.sendfile( __dirname + '/public/generic.html');
-   res.render('generic');
-});
+// app.get('/generic', (req,res) => {
+//    // res.sendfile( __dirname + '/public/generic.html');
+//    res.render('generic');
+// });
 
-app.get('/elements', (req,res) => {
-    //res.sendfile( __dirname + '/public/elements.html');
-    res.render('elements');
-});
+// app.get('/elements', (req,res) => {
+//     //res.sendfile( __dirname + '/public/elements.html');
+//     res.render('elements');
+// });
 
 
 app.listen(port, () => {
